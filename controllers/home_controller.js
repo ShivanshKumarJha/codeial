6ac3a787c225abe2798285fcbfcd7807fff804af
ShivanshKumarJha <kumarjhashivansh@gmail.com>
2023-08-12 22:41:40 +0530

    Setting up the view engine

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,8 @@
 module.exports.home = function (request, response) {
-    return response.end('<h1> Express is up for Codeial!</h1>');
+    // return response.end('<h1> Express is up for Codeial!</h1>');
+    return response.render('home',{
+        title: 'Home'
+    });
 }
 
 // module.exports.actionName=function(request,response){}
