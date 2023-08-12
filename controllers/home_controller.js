781250954bd96164d0e71ad09076b0e547233b6d
ShivanshKumarJha <kumarjhashivansh@gmail.com>
2023-08-12 18:29:42 +0530

    After setting up the controllers and routes

---
+++ controllers/home_controller.js
@@ -0,0 +1,5 @@
+module.exports.home = function (request, response) {
+    return response.end('<h1> Express is up for Codeial!</h1>');
+}
+
+// module.exports.actionName=function(request,response){}
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
-const port= 8000;
+const port = 8000;
 
-app.listen(port,function(err){
-    if(err){
+// use express router
+app.use('/', require('./routes'));
+
+app.listen(port, function (err) {
+    if (err) {
         console.log(`Error in running the server: ${err}`);
     }
     console.log(`Server is running on port: ${port}`);
