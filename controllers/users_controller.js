ec509dc658bfabbae23929ed4c7116f5dd99edc0
ShivanshKumarJha <kumarjhashivansh@gmail.com>
2023-08-18 09:04:43 +0530

    Adding partials

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
 module.exports.profile = function(request,response){
-    return response.end('<h1>Users Profile</h1>');
+    // return response.end('<h1>Users Profile</h1>');
+    return response.render('user_profile',{
+        title: 'User Profile'
+    });
 }
