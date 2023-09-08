b8ee7238192c6a95b0c68f02e7e289f9fb8a4a23
ShivanshKumarJha <kumarjhashivansh@gmail.com>
2023-09-08 18:29:09 +0530

    Adding sign-up data in controller

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 module.exports.profile = function(request,response){
     // return response.end('<h1>Users Profile</h1>');
     return response.render('user_profile',{
@@ -5,23 +7,42 @@ module.exports.profile = function(request,response){
     });
 }
 
-// Render the sign up page
+// Render the sign-up page
 module.exports.signUp = function(request,response){
     return response.render('user_sign_up',{
         title:"Codeial | Sign Up"
     })
 }
 
-// Render the sign in page
+// Render the sign-in page
 module.exports.signIn = function(request,response){
     return response.render('user_sign_in',{
         title:"Codeial | Sign In"
     })
 }
 
-// Get the sign up data
-module.exports.create = function(request,response){
-//     TODO
+// Get the sign-up data
+module.exports.create = async function(request,response){
+    if(request.body.password !== request.body.confirm_password){
+        return response.redirect('back');
+    }
+
+    try{
+        const user = await User.findOne({email:request.body.email});
+        if(!user){
+            return response.redirect('/users/sign-in');
+        }
+        else{
+            // User already exists, redirect back
+            return response.redirect('back');
+        }
+    }
+    catch(err){
+        console.error('Error:', err.message);
+        // Handle the error appropriately, e.g., send an error response
+        return response.status(500).send('Internal Server Error');
+    }
+
 }
 
 // Get the sign in data and create a session
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,5 @@ router.get('/profile',usersController.profile);
 
 router.get('/sign-up',usersController.signUp);
 router.get('/sign-in',usersController.signIn);
+router.post('/create',usersController.create);
 module.exports = router;
