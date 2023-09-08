119cd37b3e0cb59f803c18b8366a67ef63ab2d7e
ShivanshKumarJha <kumarjhashivansh@gmail.com>
2023-09-08 10:31:46 +0530

    Rendering the pages for signUp and signIn

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,3 +4,17 @@ module.exports.profile = function(request,response){
         title: 'User Profile'
     });
 }
+
+// Render the sign up page
+module.exports.signUp = function(request,response){
+    return response.render('user_sign_up',{
+        title:"Codeial | Sign Up"
+    })
+}
+
+// Render the sign in page
+module.exports.signIn = function(request,response){
+    return response.render('user_sign_in',{
+        title:"Codeial | Sign In"
+    })
+}
