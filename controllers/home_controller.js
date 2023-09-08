fa92fb5bf3caa43981f59d8fe8e5d49dd6786b26
ShivanshKumarJha <kumarjhashivansh@gmail.com>
2023-09-08 12:23:42 +0530

    Adding and updating the cookie

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,8 @@
 module.exports.home = function (request, response) {
+    // Adding cookie - in Application Updating cookie - here
+    // console.log(request.cookies);
+    // response.cookie('user_id',25);
+
     // return response.end('<h1> Express is up for Codeial!</h1>');
     return response.render('home',{
         title: 'Home'
