5f823800cad3506afe066954c429cdbe9c4ce2b1
ShivanshKumarJha <kumarjhashivansh@gmail.com>
2023-09-23 17:59:35 +0530

    Saving posts to DB

---
+++ controllers/posts_controller.js
@@ -0,0 +1,15 @@
+const Post = require('../models/post')
+
+module.exports.create = async function (req, res) {
+    try{
+      const post = await Post.create({
+        content: req.body.content,
+        user: req.user._id,
+      });
+      return res.redirect('back');
+    } 
+    catch(err){
+      console.error('Error in creating a post', err);
+      return;
+    }
+  };
