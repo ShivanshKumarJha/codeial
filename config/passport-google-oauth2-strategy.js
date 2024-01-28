9e8228d12f59446adbb3e82ea3de8b6c92db719d
ShivanshKumarJha <kumarjhashivansh@gmail.com>
2024-01-28 17:14:39 +0530

    add google auth

---
+++ config/passport-google-oauth2-strategy.js
@@ -0,0 +1,44 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const crypto = require('crypto');
+
+const User = require('../models/user');
+
+// tell passport to use a new strategy for google login
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID:
+        '487633727199-6i37vsccq1na0b3rm4hb7dtdo7oe1i6k.apps.googleusercontent.com',
+      clientSecret: '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',
+      callbackURL: 'https://localhost:8000/users/auth/google/callback',
+    },
+    async function (accessToken, refreshToken, profile, done) {
+      try {
+        // find the user
+        const user = await User.findOne({ email: profile.emails[0].value });
+
+        console.log(profile);
+
+        if (user) {
+          // if found, set this user as req.user
+          return done(null, user);
+        } else {
+          // if not found, create the user and set it as req.user
+          const newUser = await User.create({
+            name: profile.displayName,
+            email: profile.emails[0].value,
+            password: crypto.randomBytes(20).toString('hex'),
+          });
+
+          return done(null, newUser);
+        }
+      } catch (err) {
+        console.log('error in google strategy-passport', err);
+        return done(err, null);
+      }
+    }
+  )
+);
+
+module.exports = passport;
--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,19 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const usersController = require('..//controllers/users_controller');
+const { fail } = require('assert');
 
-router.get('/profile/:id', passport.checkAuthentication, usersController.profile);
-router.post('/update/:id', passport.checkAuthentication, usersController.update);
+router.get(
+  '/profile/:id',
+  passport.checkAuthentication,
+  usersController.profile
+);
+
+router.post(
+  '/update/:id',
+  passport.checkAuthentication,
+  usersController.update
+);
 
 router.get('/sign-up', usersController.signUp);
 router.get('/sign-in', usersController.signIn);
@@ -18,5 +28,16 @@ router.post(
 );
 
 router.get('/sign-out', usersController.destroySession);
+
+// For google authentication
+router.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+router.get(
+  '/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/users/sign-in' }),
+  usersController.createSession
+);
 
 module.exports = router;
