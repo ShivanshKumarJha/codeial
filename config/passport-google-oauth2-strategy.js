691fcea9e98c53150dc3654f188a031c66e64220
ShivanshKumarJha <kumarjhashivansh@gmail.com>
2024-04-25 21:30:12 +0530

    update google callback #update2

--- config/passport-google-oauth2-strategy.js
+++ config/passport-google-oauth2-strategy.js
@@ -11,8 +11,7 @@ passport.use(
     {
       clientID: '487633727199-6i37vsccq1na0b3rm4hb7dtdo7oe1i6k.apps.googleusercontent.com',
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: process.env.NODE_ENV === 'production' ?
-        process.env.PROD_URL : process.env.LOCAL_URL
+      callbackURL: process.env.PROD_URL
 
     },
     async function(accessToken, refreshToken, profile, done) {
